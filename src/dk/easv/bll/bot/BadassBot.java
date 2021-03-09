8e1dbe412bb5fbc1a0fa024acf92d1e2eb44b984
LindholmGithub <christianlindholm@jubii.dk>
2021-03-09 14:45:59 +0100

    Added Jeppes version of Prio List AI on steroids

---
+++ src/dk/easv/bll/bot/BadassBot.java
@@ -0,0 +1,93 @@
+package dk.easv.bll.bot;
+
+import dk.easv.bll.game.GameState;
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BadassBot extends LocalPrioritisedListBot {
+    private static final String BOTNAME = "BadassBot";
+
+    @Override
+    public IMove doMove(IGameState state) {
+        List<IMove> winMoves = getWinningMoves(state);
+        if(!winMoves.isEmpty())
+            return winMoves.get(0);
+
+        return super.doMove(state);
+    }
+
+
+    private boolean isWinningMove(IGameState state, IMove move, String player){
+        String[][] board = state.getField().getBoard();
+        boolean isRowWin = true;
+        // Row checking
+        int startX = move.getX()-(move.getX()%3);
+        int endX = startX + 2;
+        for (int x = startX; x <= endX; x++) {
+            if(x!=move.getX())
+                if(!board[x][move.getY()].equals(player))
+                    isRowWin = false;
+        }
+
+        boolean isColumnWin=true;
+        // Column checking
+        int startY = move.getY()-(move.getY()%3);
+        int endY = startY + 2;
+        for (int y = startY; y <= endY; y++) {
+            if(y!=move.getY())
+                if(!board[move.getX()][y].equals(player))
+                    isColumnWin = false;
+        }
+
+
+        boolean isDiagWin = true;
+
+        // Diagonal checking left-top to right-bottom
+        if(!(move.getX()==startX && move.getY()==startY))
+            if(!board[startX][startY].equals(player))
+                isDiagWin=false;
+        if(!(move.getX()==startX+1 && move.getY()==startY+1))
+            if(!board[startX+1][startY+1].equals(player))
+                isDiagWin=false;
+        if(!(move.getX()==startX+2 && move.getY()==startY+2))
+            if(!board[startX+2][startY+2].equals(player))
+                isDiagWin=false;
+
+        boolean isOppositeDiagWin = true;
+        // Diagonal checking left-bottom to right-top
+        if(!(move.getX()==startX && move.getY()==startY+2))
+            if(!board[startX][startY+2].equals(player))
+                isOppositeDiagWin=false;
+        if(!(move.getX()==startX+1 && move.getY()==startY+1))
+            if(!board[startX+1][startY+1].equals(player))
+                isOppositeDiagWin=false;
+        if(!(move.getX()==startX+2 && move.getY()==startY))
+            if(!board[startX+2][startY].equals(player))
+                isOppositeDiagWin=false;
+
+        return isColumnWin || isDiagWin || isOppositeDiagWin || isRowWin;
+    }
+
+    // Compile a list of all available winning moves
+    private List<IMove> getWinningMoves(IGameState state){
+        String player = "1";
+        if(state.getMoveNumber()%2==0)
+            player="0";
+
+        List<IMove> avail = state.getField().getAvailableMoves();
+
+        List<IMove> winningMoves = new ArrayList<>();
+        for (IMove move:avail) {
+            if(isWinningMove(state,move,player))
+                winningMoves.add(move);
+        }
+        return winningMoves;
+    }
+
+    @Override
+    public String getBotName() {
+        return BOTNAME;
+    }
+}
